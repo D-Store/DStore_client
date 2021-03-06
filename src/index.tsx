686c23f5f1cc3b17812a63ce38c400087b3f3b7b
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-03-06 15:16:01 +0900

    ADD clean tempalte

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from 'App';
 import {} from 'redux';
-import { Provider } from 'react-redux';
+import {} from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
