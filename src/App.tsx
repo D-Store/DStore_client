3f6510f75228b3ce69ecc517e2ed46a38ca6254d
조민수 <chominsu10@gmail.com>
2021-05-13 21:56:00 +0900

    [ADD] Router, RecoilRoot

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <RecoilRoot>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div className="App">hi</div>
+          </Route>
+        </Switch>
+      </Router>
+    </RecoilRoot>
+  );
 }
 
 export default App;
