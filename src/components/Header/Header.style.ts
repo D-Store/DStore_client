28421e6be618f3203877f230ce1f17422937c105
HongJunHyeok <edb1631@naver.com>
2021-05-26 15:30:35 +0900

    [ADD] Content Padding

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 import pallete from "../../style/pallete";
-import colors from "../../style/pallete";
+import sizes from "../../style/sizes";
 
 export const HeaderContainer = styled.header`
   width: 100%;
   height: 150px;
-  background-color: ${colors.background_color};
+  background-color: ${pallete.background_color};
   display: flex;
   align-items: center;
   justify-content: space-around;
   flex-direction: column;
+  max-width: ${sizes.max_width};
+  margin: 0 auto;
+  margin-bottom: 100px;
   .logo {
     width: 150px;
     height: 90px;
@@ -20,7 +23,7 @@ export const HeaderNav = styled.ul`
   display: flex;
   justify-content: center;
   width: 450px;
-  margin-top:50px
+  margin-top: 50px;
 `;
 
 interface IHeaderNavItem {
@@ -38,9 +41,8 @@ export const HeaderNavItem = styled.li<IHeaderNavItem>`
     border-radius: 3rem;
     background-color: ${(props) => props.isActive && pallete.text_color};
     text-decoration: none;
-    color:${pallete.text_color};
+    color: ${pallete.text_color};
     color: ${(props) => props.isActive && pallete.background_color};
-    font-weight: ${(props)=> props.isActive && 1000};
+    font-weight: ${(props) => props.isActive && "bold"};
   }
-
 `;
--- src/components/PageTemplate/PageTemplate.style.tsx
+++ src/components/PageTemplate/PageTemplate.style.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
+import sizes from "../../style/sizes";
 
 export const PageTemplateContainer = styled.div`
   .PageTemplate-content {
     min-height: 80vh;
+    max-width: ${sizes.max_width};
+    margin: 0 auto;
   }
 `;
