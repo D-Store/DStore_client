f0af83b27849e0bb866b92361f66220d9e0c0206
조민수 <chominsu10@gmail.com>
2021-05-31 12:10:08 +0900

    [ADD] ReadProject

---
+++ src/components/ReadProject/ReadProject.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { setUrl } from "../../lib/setUrl";
+import { ProjectType } from "../../types/project";
+
+const ReadProject: React.FC = () => {
+  return (
+    <div>
+      <div>a</div>
+    </div>
+  );
+};
+
+export default ReadProject;
---
+++ src/pages/project.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const project: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>메인 | 디스토어</title>
+      </Head>
+    </>
+  );
+};
+
+export default project;
