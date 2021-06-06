2c5e2ad95594e4d7de9b16b46ba95f15d5f6b63a
조민수 <chominsu10@gmail.com>
2021-06-07 08:56:11 +0900

    [UPDATE] hashTag

--- src/components/ReadProjectContent/ReadProjectContent.style.tsx
+++ src/components/ReadProjectContent/ReadProjectContent.style.tsx
@@ -2,6 +2,18 @@ import styled from "styled-components";
 import sizes from "../../style/sizes";
 
 export const ReadProjectContentContainer = styled.div`
-  max-width: 900px;
+  max-width: 1200px;
   min-height: ${sizes.project_height};
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  .content {
+    max-width: 900px;
+  }
+  .hashtag {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    max-width: 200px;
+  }
 `;
