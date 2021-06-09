0b91e043f2116503dbd8192d4b24cbac7fae98b1
조민수 <chominsu10@gmail.com>
2021-06-09 19:55:37 +0900

    [ADD] writeForm

--- src/components/WriteForm/styles.tsx
+++ src/components/WriteForm/styles.tsx
@@ -1,3 +1,47 @@
 import styled from "styled-components";
+import pallete from "../../style/pallete";
+import sizes from "../../style/sizes";
 
-export const WriteFormContainer = styled.form``;
+export const WriteFormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 1620px;
+  align-items: center;
+`;
+
+export const WriteFormContext = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 1000px;
+  height: 1620px;
+  background-color: ${pallete.input_back};
+  input {
+    width: 800px;
+    background-color: ${pallete.input_back};
+    border: 0;
+    padding-top: 100px;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid ${pallete.bright};
+    transition: 0.5s cubic-bezier(0, 1, 1, 1);
+    outline: none;
+    font-size: ${sizes.title_font};
+    color: ${pallete.text_color};
+  }
+  input:focus {
+    border-bottom: 3px solid ${pallete.bright};
+    transition: ${`0.5s cubic-bezier(0, ${Math.random() * 10}, 1, 10)`};
+  }
+  textarea {
+    width: 800px;
+    height: 100%;
+    padding-top: 1rem;
+    background-color: ${pallete.input_back};
+    resize: none;
+    border: 0;
+    outline: none;
+    font-size: ${sizes.mideum_font};
+    color: ${pallete.text_color};
+    font-family: Noto Sans, Noto Sans KR;
+  }
+`;
--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -5,15 +5,6 @@ import { pageInit } from "../lib/pageInit";
 import WriteForm from "../components/WriteForm";
 
 const find: NextPage = () => {
-  //프로젝트 생성 시 필요한 것들
-  //   {
-  //     title:String,
-  //     content:String,
-  //     users:[Long],
-  //     file:[file],
-  //     tags:[string]
-  // }
-
   return (
     <>
       <Head>
@@ -23,11 +14,5 @@ const find: NextPage = () => {
     </>
   );
 };
-
-export const getServerSideProps = wrapper.getServerSideProps(
-  async (context) => {
-    await pageInit(context);
-  }
-);
 
 export default find;
